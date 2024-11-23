01d13c5897fc41af38704197c6bcc410b9e8e23f
CrimsonPanda <45400656+udaravidunuwan@users.noreply.github.com>
2024-11-23 15:00:25 +0530

    variable init

--- PONG GAME/Form1.Designer.cs
+++ PONG GAME/Form1.Designer.cs
@@ -29,12 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.computer = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Image = global::PONG_GAME.Properties.Resources.player;
+            this.player.Location = new System.Drawing.Point(12, 139);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(30, 120);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // computer
+            // 
+            this.computer.Image = global::PONG_GAME.Properties.Resources.computer;
+            this.computer.Location = new System.Drawing.Point(758, 139);
+            this.computer.Name = "computer";
+            this.computer.Size = new System.Drawing.Size(30, 120);
+            this.computer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.computer.TabIndex = 1;
+            this.computer.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.Image = global::PONG_GAME.Properties.Resources.ball;
+            this.ball.Location = new System.Drawing.Point(385, 165);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.computer);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Player: 0 -- Computer:0";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox computer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
--- PONG GAME/Form1.cs
+++ PONG GAME/Form1.cs
@@ -12,9 +12,48 @@ namespace PONG_GAME
 {
     public partial class Form1 : Form
     {
+        int ballXSpeed = 4;
+        int ballYSpeed = 5;
+        int speed = 2;
+        Random rand = new Random();
+        bool goDown, goUp;
+        int computerSpeedChange = 50;
+        int playerScore = 0;
+        int computerScore = 0;
+        int playerSpeed = 8;
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void CheckCollision(PictureBox picOne, PictureBox picTwo, int offset) 
+        {
+            
+        } 
+
+        private void gameOver(string message)
+        {
+            
+        }
+
+
+
     }
 }
